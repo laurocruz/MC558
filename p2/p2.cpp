e27487423a2e51d990fcbffd41cfa05d67622f40
Lauro Cruz <laurocruzsouza@gmail.com>
2017-04-16 17:55:40 -0300

    p2 done

--- p2/p2.cpp
+++ p2/p2.cpp
@@ -1,8 +1,42 @@
+// Lauro Cruz e Souza - 156175
+// lauro.souza@students.ic.unicamp.br | laurocruzsouza@gmail.com
+
+/* A ideia do laboratório é encontrar a capital mais próxima de cada cidade comum 
+ * e o caminho entre elas. Assim, tendo esses caminhos mínimos, podemos definir quais 
+ * são as ferrovias essenciais para que esses caminhos sejam mantidos, como foi
+ * pedido no enunciado.
+ *
+ * A ideia da resolução, como foi sugerida no enunciado, pode ser pensada inicialmente 
+ * para uma única capital e extrapolada para várias. No caso de uma capital, podemos
+ * aplicar normalmente o algoritmo de dijkstra com a raiz da árvore sendo a capital,
+ * assim qualquer ferrovia não presente na árvore de distâncias mínimas pode ser 
+ * desativada.
+ *
+ * Para várias capitais, podemos imaginar a existência de uma única capital superior
+ * que é conectada apenas a todas as outras capitais com estradas de distância 0 e
+ * chamar a função de dijkstra com essa capital como raiz. Assim podemos garantir 
+ * que cada caminho de uma cidade à capital é à capital mais próxima, pois, se 
+ * chamarmor a capital superior de CF e as inferiores de CEi, sendo i o número da
+ * capital, o caminho CF--C é mínimo, ou seja, CF-CEi--C é mínimo. Assim, se existe j
+ * para o qual d(CEi--C) > d(CEj--C), então d(CF-CEi--C) > d(CF-CEj--C), tendo assim 
+ * uma contradição.
+ *
+ * Para simular a existência desta capital superior, podemos apenas colocar as 
+ * distâncias das capitais como 0 e rodar o algoritmo de dijkstra começando por uma
+ * delas.
+ *
+ * Outras condição a ser verificada é se, entre dois caminhos de mesma distância a duas 
+ * capitais diferente, fazer o relax de v (u-v) se a aresta entre u e v for uma estrada.
+ * Isso é importante pois, entre uma cidade e sua capital mais próxima pode existir 
+ * apenas uma ferrovia, pois pelo menos uma das pontas de cada ferrovia é uma capital.
+ * Assim, é importante fazer o relax, pois caso o pai de v fosse uma ferrovia, estaremos
+ * retirando uma das ferrovias da árvore de caminhos mínimos.
+*/
+
 #include <cstdio>
 #include <climits>
 #include <iostream>
 #include <vector>
-#include <queue>
 #include <set>
 
 using namespace std;
@@ -31,19 +65,11 @@ class Graph {
     // Lista de adjacência
     vector<struct ver> *vertex; 
 
+    // Cria aresta
     struct edge make_edge (int v, int peso) {
         struct edge e = {v, peso};
         return e;
     };
-
-    /*
-       void relax (int * dv, int * du, int u, int wv, pair<int,bool> * piv, bool ferrovia) {
-       if (*dv > *du + wv) {
-     *dv = *du + wv;
-     *piv = make_pair(u, ferrovia);
-     }
-     }
-     */
 
     public:
     // Construtor
@@ -52,25 +78,34 @@ class Graph {
     // Cria aresta entre vértices i e j
     void addEdge(int i, int j, bool ferrovia, int peso);
 
+    // Função que utiliza Dijkstra para definir ferrovias a serem desativadas
     int removeFerrovias();
 
 };
 
+// Construtor
+// n : Numero de vértices
+// f: Número de ferrovias
 Graph::Graph(int n, int f) {
     this->n = n;
     this->f = f;
     this->vertex = new vector<struct ver>(n);
 
+    // Inicialmente nenhuma das cidades é capital
     for (int i = 0; i < n; i++)
         (*vertex)[i].capital = false;
 }
 
+// Adiciona nova aresta (estrada ou ferrovia
+// (i, j): aresta
+// ferrovia: true se sim, false caso contrário
+// peso: distância (peso) da estrada/ferrovia (aresta)
 void Graph::addEdge(int i, int j, bool ferrovia, int peso) {
-
     if (ferrovia) {
         (*vertex)[i].ferrovias.push_back(make_edge(j, peso));
         (*vertex)[j].ferrovias.push_back(make_edge(i, peso));
 
+        // primeiro vértice (i) da aresta da ferrovia é capital
         (*vertex)[i].capital = true;
     } else {
         (*vertex)[i].estradas.push_back(make_edge(j, peso));
@@ -83,9 +118,6 @@ int Graph::removeFerrovias() {
     int fers = this->f;
     // Vetor de distâncias
     vector<int> d(this->n, INT_MAX);
-
-    // Vetor de quantidade de ferrovias até cidade
-    vector<int> n_fers(this->n, 0);
 
     //Vetor dos pais. Aresta = (pi[x], x)
     //.first: pai de i
@@ -109,16 +141,6 @@ int Graph::removeFerrovias() {
         Q.insert(make_pair(d[i],i));
     }
 
-    /*
-    if (this->n < 100) {
-        set< pair<int,int> >::iterator is;
-        for (is = Q.begin(); is != Q.end(); is++) {
-            cout << (*is).second+1 << ": " << (*is).first << endl;
-        }
-        cout << endl;
-    }
-    */
-
     while (!Q.empty()) {
         // u - Vertice a ser avaliado
         u = (*(Q.begin())).second;
@@ -133,28 +155,19 @@ int Graph::removeFerrovias() {
             int c = (*it).peso;
 
             // relax
-            if (d[v] > d[u] + c) {
-                // Atualiza distância
-                if (inQ[v])
-                    Q.erase(Q.find(make_pair(d[v],v)));
+            if (d[v] >= d[u] + c && inQ[v]) {
+                int dv = d[v];
 
+                Q.erase(Q.find(make_pair(dv,v)));
+
+                // Atualiza distância
                 d[v] = d[u] + c;
-                n_fers[v] = n_fers[u];
+                // Atualiza pai de v (u)
                 pi[v].first = u;
+                // v e u são ligador por estrada
                 pi[v].second = false;
 
                 Q.insert(make_pair(d[v],v));
-                inQ[v] = true;
-
-            } else if ((d[v] == d[u] + c) && (n_fers[v] > n_fers[u])) {
-                if (!inQ[v]) {
-                    Q.insert(make_pair(d[v],v));
-                    inQ[v] = true;
-                }
-
-                n_fers[v] = n_fers[u];
-                pi[v].first = u;
-                pi[v].second = false;
             }
         }
 
@@ -165,46 +178,25 @@ int Graph::removeFerrovias() {
             int c = (*it).peso;
 
             // relax
-
             if (d[v] > d[u] + c) {
+                
+                Q.erase(Q.find(make_pair(d[v],v)));
                 // Atualiza distância
-                if (inQ[v])
-                    Q.erase(Q.find(make_pair(d[v],v)));
-
                 d[v] = d[u] + c;
-                n_fers[v] = n_fers[u] + 1;
+                // Atualiza pai de v (u)
                 pi[v].first = u;
+                // v e u são ligados por ferrovia
                 pi[v].second = true;
 
                 Q.insert(make_pair(d[v],v));
-                inQ[v] = true;
-
-            } else if ((d[v] == d[u] + c) && (n_fers[v] > n_fers[u] + 1)) {
-                if (!inQ[v]) {
-                    Q.insert(make_pair(d[v],v));
-                    inQ[v] = true;
-                }
-
-                n_fers[v] = n_fers[u] + 1;
-                pi[v].first = u;
-                pi[v].second = true;
-
             }
         }
-        /*
-        if (this->n < 100) {
-            set< pair<int,int> >::iterator is;
-            for (is = Q.begin(); is != Q.end(); is++) {
-                cout << (*is).second+1 << ": " << (*is).first << endl;
-            }
-            cout << endl;
-        }
-        */
-
     }
 
-    for (int i = 0; i < n; i++) {
-        if (pi[i].second)
+    // Todas as ferrovias não presentes na árvore de distâncias
+    // mínimas podem ser retiradas
+    for (int i = 0; i < this->n; i++) {
+        if (pi[i].second) 
             fers--;
     }
 
